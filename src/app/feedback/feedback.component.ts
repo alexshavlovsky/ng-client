5f78564cc88986a89cd19ad21fb78eae177ade43
alexshavlovsky <ctzn@tut.by>
2019-06-12 01:32:31 +0300

    refactor feedback component

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -9,7 +9,7 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./feedback.component.scss']
 })
 export class FeedbackComponent implements OnInit {
-  feedbackForm: FormGroup;
+  from: FormGroup;
   submitted = false;
   btnDisabled = false;
   submitFailed = false;
@@ -20,7 +20,7 @@ export class FeedbackComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.feedbackForm = this.formBuilder.group({
+    this.from = this.formBuilder.group({
       senderName: ['', Validators.required],
       senderEmail: ['', [Validators.required, Validators.email]],
       feedbackText: ['', Validators.required]
@@ -28,41 +28,38 @@ export class FeedbackComponent implements OnInit {
   }
 
   get nameErrors() {
-    const err = this.feedbackForm.controls.senderName.errors;
+    const err = this.from.controls.senderName.errors;
     return {hasErrors: this.submitted && err !== null, errors: err};
   }
 
   get emailErrors() {
-    const err = this.feedbackForm.controls.senderEmail.errors;
+    const err = this.from.controls.senderEmail.errors;
     return {hasErrors: this.submitted && err !== null, errors: err};
   }
 
   get feedbackErrors() {
-    const err = this.feedbackForm.controls.feedbackText.errors;
+    const err = this.from.controls.feedbackText.errors;
     return {hasErrors: this.submitted && err !== null, errors: err};
   }
 
   onSubmit(): void {
     this.submitted = true;
-    if (this.feedbackForm.invalid) {
+    if (this.from.invalid) {
       return;
     }
     this.btnDisabled = true;
     this.submitFailed = false;
-    this.api.postFeedback(this.feedbackForm.value).subscribe(
+    this.api.postFeedback(this.from.value).subscribe(
       res => {
         this.toast.success(res.message);
-        this.feedbackForm.setValue({senderName: '', senderEmail: '', feedbackText: ''});
+        this.from.setValue({senderName: '', senderEmail: '', feedbackText: ''});
         this.submitted = false;
         this.btnDisabled = false;
         this.submitFailed = false;
       },
       err => {
-        if (err.error.error && err.error.errors) {
-          this.toast.error(err.error.error);
-          for (const e of err.error.errors) {
-            this.toast.warning(e.defaultMessage, e.field);
-          }
+        if (err.error.message) {
+          this.toast.error(err.error.message);
         } else {
           this.toast.error('Failed to send feedback');
         }
